b5a792674abaaa68e8fbe6d62f90cd13b8473f5c
LakshyaRunthala <161014790+LakshyaRunthala@users.noreply.github.com>
2025-07-22 20:06:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -26,6 +26,12 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.error(err));
 
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'register.html'));
+});
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log('Server running on port :',PORT);
